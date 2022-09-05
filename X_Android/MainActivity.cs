4c3cacf61339d20983b252c37546a381bd2a05e0
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-09-05 14:05:21 +0200

    Xamarin.Andoid

--- X_Android/MainActivity.cs
+++ X_Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace X_Android
@@ -8,12 +10,33 @@ namespace X_Android
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        public EditText Edt_Input { get; set; }
+        public Button Btn_Ok { get; set; }
+        public Button Btn_Google { get; set; }
+        public Button Btn_ShowPicture { get; set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Btn_Ok = FindViewById<Button>(Resource.Id.activity_main_Btn_Ok);
+            Edt_Input = FindViewById<EditText>(Resource.Id.activity_main_Edt_Input);
+
+            Btn_Ok.Click += (s, e) => Toast.MakeText(this, $"Ihre ausgewählte Zahl ist {Edt_Input.Text}.", ToastLength.Short).Show();
+            Edt_Input.Click += (s, e) => Toast.MakeText(this, $"Ihre ausgewählte Zahl ist {Edt_Input.Text}.", ToastLength.Short).Show();
+
+            Btn_Google = FindViewById<Button>(Resource.Id.activity_main_Btn_Google);
+            Btn_ShowPicture = FindViewById<Button>(Resource.Id.activity_main_Btn_ShowPicture);
+
+            Intent impliziterIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("https://www.google.de"));
+            Btn_Google.Click += (s, e) => StartActivity(impliziterIntent);
+
+            Intent expliziterIntent = new Intent(this, typeof(ShowPictureActivity));
+            Btn_ShowPicture.Click += (s,e) => StartActivity(expliziterIntent);
+        
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ X_Android/ShowPictureActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X_Android
+{
+    [Activity(Label = "ShowPictureActivity")]
+    public class ShowPictureActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_showpicture);
+        }
+    }
+}
